eabe193c261480c9b1beb9b1199a460435cfd76d
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2020-04-13 03:25:36 -0300

    add vector norm method

--- vector2d.go
+++ vector2d.go
@@ -1,6 +1,9 @@
 package qlearning
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Vec2D struct {
 	X int
@@ -21,6 +24,11 @@ func (v Vec2D) Add(u Vec2D) Vec2D {
 
 func (v Vec2D) Subtract(u Vec2D) Vec2D {
 	return Vec2D{X: v.X - u.X, Y: v.Y - u.Y}
+}
+
+func (v Vec2D) Norm() float64 {
+	x, y := float64(v.X), float64(v.Y)
+	return math.Sqrt(x*x + y*y)
 }
 
 func (v Vec2D) String() string {
