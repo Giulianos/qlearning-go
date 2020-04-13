0dae9b731b6667d02b82a377d28104674b6756a3
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2020-04-13 03:22:50 -0300

    expose agent state

--- agent.go
+++ agent.go
@@ -6,6 +6,11 @@ type Agent struct {
 	env *Environment
 }
 
+type AgentState struct {
+	Dist Vec2D
+	Dir  Vec2D
+}
+
 func NewAgent(r, dir Vec2D, env *Environment) Agent {
 	return Agent{&r, &dir, env}
 }
@@ -14,7 +19,7 @@ func (a *Agent) PerformAction(action Action) {
 	switch action {
 	case Forward:
 		if a.env.CheckBoundary(*a.dir) {
-			r := a.r.Add(*a.dir)
+			r := a.r.Subtract(*a.dir)
 			a.r = &r
 			a.env.NotifyAgentMove(*a.dir)
 		}
@@ -27,5 +32,12 @@ func (a *Agent) PerformAction(action Action) {
 		dir := a.dir.RotateCCW()
 		a.dir = &dir
 		break
+	}
+}
+
+func (a Agent) GetState() AgentState {
+	return AgentState{
+		Dist: *a.r,
+		Dir:  *a.dir,
 	}
 }
