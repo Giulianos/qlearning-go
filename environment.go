2f3a4a4d10ac2473b2e8c6d4319d1eacbd9fa32c
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2020-04-13 03:23:43 -0300

    add string and goal checking to environment

--- environment.go
+++ environment.go
@@ -5,6 +5,7 @@ type Environment struct {
 	// checking and rendering
 	goalPos  *Vec2D
 	agentPos *Vec2D
+	agent *Agent
 
 	// Dimensions
 	dim *Vec2D
@@ -19,6 +20,7 @@ func GenerateEnvironment(agentPos, agentDir, goalPos, dim Vec2D) (*Environment, 
 	}
 
 	a := NewAgent(goalPos.Subtract(agentPos), agentDir, &e)
+	e.agent = &a
 
 	return &e, &a
 }
@@ -31,4 +33,33 @@ func (e *Environment) CheckBoundary(movementDir Vec2D) bool {
 func (e *Environment) NotifyAgentMove(movementDir Vec2D) {
 	agentPos := e.agentPos.Add(movementDir)
 	e.agentPos = &agentPos
+}
+
+func (e Environment) GoalAchived() bool {
+	return e.agentPos.X == e.goalPos.X && e.agentPos.Y == e.goalPos.Y
+}
+
+func (e Environment) String() string {
+	s := ""
+	agentDir := e.agent.dir
+
+	for y := 0; y < e.dim.Y; y++ {
+		for x := 0; x < e.dim.X; x++ {
+			if x == e.goalPos.X && y == e.goalPos.Y {
+				s += "X "
+			} else if x == e.agentPos.X && y == e.agentPos.Y {
+				switch *agentDir {
+				case Vec2D{0, -1}: s += "▲ "
+				case Vec2D{0, 1}: s += "▼ "
+				case Vec2D{-1, 0}: s += "◄ "
+				case Vec2D{1, 0}: s += "► "
+				}
+			} else {
+				s += "- "
+			}
+		}
+		s += "\n"
+	}
+
+	return s
 }
