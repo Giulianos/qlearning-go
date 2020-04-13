9b19b0fba238bef9b3e90ebd53909daa682e7744
Giuliano Scaglioni <scaglionigiuliano@gmail.com>
2020-04-13 03:25:54 -0300

    add simple usage example

---
+++ cmd/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Giulianos/qlearning-go"
+)
+
+func main() {
+	knowledge := qlearning.QLearning{
+		Q: make(map[qlearning.AgentState]map[qlearning.Action]float64, 0),
+	}
+
+	knowledge.Learn(
+		func(s1, s2 qlearning.AgentState) float64 {
+			if s2.Dist.Norm() < s1.Dist.Norm() {
+				return 1.0
+			} else {
+				return -1.0
+			}
+		},
+		1000,
+		func(episode int) float64 { return 0.6 },
+		1.0,
+		func(episode int) float64 { return 0.1 },
+	)
+
+	// Test 1
+	env, agent := qlearning.GenerateEnvironment(
+		qlearning.Vec2D{5, 5},   // Agent position
+		qlearning.Vec2D{0, -1},  // Agent direction (up)
+		qlearning.Vec2D{9, 9},   // Goal position
+		qlearning.Vec2D{10, 10}, // Environment size
+	)
+
+	// Print env
+	fmt.Println(env)
+
+	// Run agent (following knowledge policy)
+	knowledge.Run(env, agent)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Giulianos/qlearning-go
+
+go 1.13
+
+require github.com/aclements/go-gg v0.0.0-20170323211221-abd1f791f5ee
